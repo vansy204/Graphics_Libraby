5511458376b59f1387d78adee257fe6fc5c7b773
Văn Sỹ <0978pvs054@gmail.com>
2023-08-12 16:28:45 +0700

    Add project files.

---
+++ Graphics_Libraby/Windows.h
@@ -0,0 +1,96 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include <string>
+using namespace std;
+// hàm chỉnh sửa kích thước cửa sổ console
+void SetWindowSize(SHORT width, SHORT height)
+{
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SMALL_RECT WindowSize;
+	WindowSize.Top = 0;
+	WindowSize.Left = 0;
+	WindowSize.Right = width;
+	WindowSize.Bottom = height;
+
+	SetConsoleWindowInfo(hStdout, 1, &WindowSize);
+}
+// thay đổi kích thước hiển thị trong console
+void SetScreenBufferSize(SHORT width, SHORT height)
+{
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	COORD NewSize;
+	NewSize.X = width;
+	NewSize.Y = height;
+
+	SetConsoleScreenBufferSize(hStdout, NewSize);
+}
+// vô hiệu hóa thay đổi kích thước màn hình
+void DisableResizeWindow()
+{
+	HWND hWnd = GetConsoleWindow();
+	SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) & ~WS_SIZEBOX);
+}
+// vô hiệu hóa các nút min max close
+void DisableCtrButton(bool Close, bool Min, bool Max)
+{
+	HWND hWnd = GetConsoleWindow();
+	HMENU hMenu = GetSystemMenu(hWnd, false);
+
+	if (Close == 1)
+	{
+		DeleteMenu(hMenu, SC_CLOSE, MF_BYCOMMAND);
+	}
+	if (Min == 1)
+	{
+		DeleteMenu(hMenu, SC_MINIMIZE, MF_BYCOMMAND);
+	}
+	if (Max == 1)
+	{
+		DeleteMenu(hMenu, SC_MAXIMIZE, MF_BYCOMMAND);
+	}
+}
+
+// hàm ẩn hiện 2 thanh cuộn
+void ShowScrollbar(BOOL Show)
+{
+	HWND hWnd = GetConsoleWindow();
+	ShowScrollBar(hWnd, SB_BOTH, Show);
+}
+// hàm thay đổi màu chữ
+void SetColor(int backgound_color, int text_color)
+{
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	int color_code = backgound_color * 16 + text_color;
+	SetConsoleTextAttribute(hStdout, color_code);
+}
+// hàm dịch truyển con trỏ tới vị trí x,y với trục x năm ngang và y nằm dọc tính từ góc trên bên trái
+void GoTo(SHORT posX, SHORT posY)
+{
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD Position;
+	Position.X = posX;
+	Position.Y = posY;
+	SetConsoleCursorPosition(hStdout, Position);
+}
+// ẩn hiện con trỏ
+void ShowCur(bool CursorVisibility)
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO ConCurInf;
+
+	ConCurInf.dwSize = 10;
+	ConCurInf.bVisible = CursorVisibility;
+
+	SetConsoleCursorInfo(handle, &ConCurInf);
+}
+// vô hiệu hóa bôi đen text
+void DisableSelection()
+{
+	HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+
+	SetConsoleMode(hStdin, ~ENABLE_QUICK_EDIT_MODE);
+}
